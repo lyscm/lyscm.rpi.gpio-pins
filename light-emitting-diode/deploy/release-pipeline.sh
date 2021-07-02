065971c3bb96f9035065fdadb9d1dbeeb1855366
qcteq <qcteqcr-environment-builder@qcteq.com>
2021-07-02 09:31:31 +0000

    update

--- light-emitting-diode/deploy/release-pipeline.sh
+++ light-emitting-diode/deploy/release-pipeline.sh
@@ -7,3 +7,21 @@ CONTAINER_NAME=light-emitting-diode
 TAG=ghcr.io/$USERNAME/$CONTAINER_NAME:arm32v7
 
 docker buildx build --platform linux/arm/v7 -t $TAG .. --push
+
+
+# CONTAINER_NAME=light-emitting-diode
+# IMAGE_NAME=ghcr.io/lyscm/$CONTAINER_NAME:arm32v7
+#
+# docker stop $CONTAINER_NAME
+# docker rm $CONTAINER_NAME
+# 
+# docker pull $IMAGE_NAME
+#
+# docker run \
+#    -d \
+#    -p 8080:8000 \
+#    --device /dev/gpiomem \
+#    --name $CONTAINER_NAME \
+#    --restart=unless-stopped \
+#    --privileged \
+#    $IMAGE_NAME
