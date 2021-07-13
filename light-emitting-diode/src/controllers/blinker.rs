3afcb03477c3ffdd3f1152f93aa6eb6fd97b9cf2
lyscm <no-reply@lyscm.github.com>
2021-07-13 16:59:34 +0000

    change functionality

--- light-emitting-diode/src/controllers/blinker.rs
+++ light-emitting-diode/src/controllers/blinker.rs
@@ -1,17 +1,28 @@
+use std::{
+    thread::sleep,
+    time::{Duration, Instant},
+};
+
 use rust_gpiozero::LED;
 
-#[post("/blink/interval?<pin>&<frequency>")]
-pub async fn blink_per_interval(pin: u8, frequency: f32) {
-    let mut led = LED::new(pin);
+#[post("/blink/interval?<pin>&<frequency>&<duration>")]
+pub async fn blink_per_interval(pin: u8, frequency: u64, duration: u64) -> String {
+    let led = LED::new(pin);
 
-    format!("blinking LED {:?} times per second.", frequency);
-    led.blink(frequency, frequency);
+    let now = Instant::now();
+    let frequency_duration = Duration::new(frequency, 0);
+    while duration < now.elapsed().as_secs() {
+        led.on();
+        sleep(frequency_duration);
+        led.off();
+        sleep(frequency_duration);
+    }
+
+    format!("blinking LED {:?} times per second.", frequency)
 }
 
 #[post("/blink/count?<pin>&<amount>")]
-pub async fn blink_per_count(pin: u8, amount: i32) {
-    let mut led = LED::new(pin);
-
-    format!("blinking LED for a total amount of {:?} times.", amount);
-    led.set_blink_count(amount);
+pub async fn blink_per_count(pin: u8, amount: u64) -> String {
+    blink_per_interval(pin, 1, amount).await;
+    format!("blinking LED for a total amount of {:?} times.", amount)
 }
